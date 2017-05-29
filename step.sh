30aba0e51fdddcc95e92796dbf43862a6ccd8c11
Johnny Sørensen <joso@nodes.dk>
2017-05-29 14:51:25 +0200

    return correctly

--- step.sh
+++ step.sh
@@ -23,4 +23,4 @@ echo "Previous tag: $previous_tag"
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
-return 0
+exit 0
