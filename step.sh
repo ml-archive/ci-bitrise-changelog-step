05adb0091017b9b3e811868d773825d7d5150d84
Johnny Sørensen <joso@nodes.dk>
2017-05-29 15:23:18 +0200

    generates changelog

--- step.sh
+++ step.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
 latest_tag=`git describe --tags`
-previous_tag=`git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1``
+previous_tag="$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))"
+changelog=""
+committer="Build triggered from: $(git log --pretty=format:"%ce" HEAD^..HEAD)"
 
+if [[ $previous_tag ]] && [[ $latest_tag ]]
+then
+    changelog="$(git log --pretty=format:" - %s (%ce - %cD)" $latest_tag...$previous_tag)"    
+else
+    changelog="$(git log --pretty=format:" - %s (%ce - %cD)")"    
+fi
+
+echo "Committer: $committer"
 echo "Latest tag: $latest_tag"
 echo "Previous tag: $previous_tag"
+echo "Changelog: $changelog"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-#  envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+envman add --key COMMIT_CHANGELOG --value $changelog
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
 exit 0
