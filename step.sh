cf5de35eff38074fcb9ebe3b804ffbb7aea04efc
Johnny Sørensen <joso@nodes.dk>
2017-09-15 14:04:26 +0200

    fixed some syntax errors

--- step.sh
+++ step.sh
@@ -14,7 +14,7 @@ elif [ $all_tags = 1 ]; then
     echo "Fetching commits since first commit."
     # We have first tag, fetch since first commit (ie. don't specify previous tag)
     
-    if [ -n "${push}" -a "${markdown_output}" == "true" ]; then
+    if [ -n "${markdown_output}" -a "${markdown_output}" == "true" ]; then
         changelog="$(git log --pretty=format:" - %s (%cr) _<%ce>_")"
     else
         changelog="$(git log --pretty=format:" - %s (%cr) _<%ce>_")"
@@ -27,26 +27,27 @@ else
     previous_tag="$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))"
 
     # Get commit messages since previous tag
-    if [ -n "${push}" -a "${markdown_output}" == "true" ]; then
+    if [ -n "${markdown_output}" -a "${markdown_output}" == "true" ]; then
         changelog="$(git log --pretty=format:" - %s (%cr) _<%ce>_" $latest_tag...$previous_tag)"    
     else
         changelog="$(git log --pretty=format:"%s  (%cr) _<%ce>_" $latest_tag...$previous_tag)"    
     fi
-
 fi
 
 # Add branch info
 NEWLINE=$'\n'
-if [ -n "${BITRISE_GIT_BRANCH}" -a "${BITRISE_GIT_BRANCH}" == *"feature"* ]; then
-    branchinfo="*_WARNING_*: This is a _FEATURE_ build on *${BITRISE_GIT_BRANCH}*${NEWLINE}" 
-    changelog=$branchinfo$changelog
-elif [ -n "${BITRISE_GIT_BRANCH}" -a "${BITRISE_GIT_BRANCH}" == *"hotfix"* ]; then
-    branchinfo="*_WARNING_*: This is a _HOTFIX_ build on *${BITRISE_GIT_BRANCH}*${NEWLINE}"
-    changelog=$branchinfo$changelog
-else
-    branchinfo="Built on *${BITRISE_GIT_BRANCH}*${NEWLINE}"
-    changelog=$branchinfo$changelog
-if
+if [ -n "${BITRISE_GIT_BRANCH}" ]; then
+    if [[ "${BITRISE_GIT_BRANCH}" == *"feature"* ]]; then
+        branchinfo="*_WARNING_*: This is a _FEATURE_ build on *${BITRISE_GIT_BRANCH}*${NEWLINE}" 
+        changelog=$branchinfo$changelog
+    elif [[ "${BITRISE_GIT_BRANCH}" == *"hotfix"* ]]; then
+        branchinfo="*_WARNING_*: This is a _HOTFIX_ build on *${BITRISE_GIT_BRANCH}*${NEWLINE}"
+        changelog=$branchinfo$changelog
+    else
+        branchinfo="Built on *${BITRISE_GIT_BRANCH}*${NEWLINE}"
+        changelog=$branchinfo$changelog
+    fi
+fi
 
 # Output collected information
 echo "Committer: $(git log --pretty=format:"%ce" HEAD^..HEAD)"
